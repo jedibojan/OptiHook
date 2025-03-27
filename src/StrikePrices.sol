4c9e7f1853e3cee3637a6b00044758ca8ed7698e
jedibojan <jedibojan@gmail.com>
2025-03-27 21:32:18 +0100

    strike prices implementation for first time period and checking the results

--- src/StrikePrices.sol
+++ src/StrikePrices.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.26;
 
-import "forge-std/console.sol";
+import "forge-std/console2.sol";
 
 /**
  * @title StrikePrices
@@ -35,22 +35,25 @@ library StrikePrices {
         uint32 timeToExpiration = expirationTime - currentTimestamp;
         uint8 category = _determineTimeCategory(timeToExpiration);
 
-        // Get standard increments and find closest to target
-        uint256 target = currentPrice / 50;
-        uint256 baseIncrement = _findClosestIncrement(target);
-        console.log("baseIncrement", baseIncrement);
+        // Get lower and upper bounds of strike price
+        (uint256 minStrike, uint256 maxStrike) = _calculateStrikeRange(currentPrice, category);
 
-        // // Apply category multiplier
-        // uint256 increment = baseIncrement * _getCategoryMultiplier(category);
+        // Calculate target as (maxStrikePrice - minStrikePrice) / 15
+        uint256 target = (maxStrike - minStrike) / 15;
+        uint256 increment = _findClosestIncrement(target);
 
-        // // Calculate strike range
-        // (uint256 minStrike, uint256 maxStrike) = _calculateStrikeRange(currentPrice, category);
+        // Calculate starting strike price (nearest to current price)
+        uint256 startStrike = _round(currentPrice, increment);
+        minStrike = _round(minStrike, increment);
+        maxStrike = _round(maxStrike, increment);
 
-        // // Calculate starting strike
-        // uint256 startStrike = (currentPrice / increment) * increment;
+        // For Category 0, use special strike price generation
+        if (category == 0) {
+            return _generateCategory0StrikePrices(startStrike, minStrike, maxStrike, increment);
+        }
 
-        // // Generate final strike prices
-        // return _generateStrikePrices(startStrike, increment, minStrike, maxStrike);
+        // For other categories, use standard generation
+        return _generateStrikePrices(startStrike, increment, minStrike, maxStrike);
     }
 
     function _determineTimeCategory(uint32 timeToExpiration) internal pure returns (uint8) {
@@ -72,31 +75,33 @@ library StrikePrices {
         pure 
         returns (uint256 baseIncrement) 
     {
-        uint256[] memory standardIncrements = new uint256[](4);
+        uint256[] memory standardIncrements = new uint256[](3);
         standardIncrements[0] = 25;
         standardIncrements[1] = 50;
         standardIncrements[2] = 100;
-        standardIncrements[3] = 200;
-        
-        uint16 pwr = 1;
-        console.log("target", target);
-        console.log("target % 1000", target % 1000);
-        while (target % 1000 == 0) {
-            pwr++;
-            console.log("pwr", pwr);
-            target /= 10;
+
+        // Calculate initial target interval
+        uint256 targetInterval = target;
+        while (targetInterval > standardIncrements[2]) {
+            standardIncrements[0] *= 10;
+            standardIncrements[1] *= 10;
+            standardIncrements[2] *= 10;
         }
-        // console.log("pwr", pwr);
-        console.log("target after", target);
-        uint256 minDiff = type(uint256).max;
-        for (uint256 i = 0; i < standardIncrements.length; i++) {
-            uint256 scaledIncrement = standardIncrements[i] * 10**pwr;
-            uint256 diff = scaledIncrement > target 
-                ? scaledIncrement - target 
-                : target - scaledIncrement;
-            if (diff < minDiff) {
-                minDiff = diff;
-                baseIncrement = scaledIncrement;
+
+        uint256 minDiff = targetInterval;
+        uint256 length = standardIncrements.length;
+        if (targetInterval < standardIncrements[0]) {
+            baseIncrement = standardIncrements[0];
+        } else {            
+            for (uint256 i = 0; i < length; i++) {
+                uint256 scaledIncrement = standardIncrements[i];
+                uint256 diff = scaledIncrement > targetInterval 
+                    ? scaledIncrement - targetInterval 
+                    : targetInterval - scaledIncrement;
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    baseIncrement = scaledIncrement;
+                }
             }
         }
     }
@@ -131,22 +136,69 @@ library StrikePrices {
         uint256 minStrike,
         uint256 maxStrike
     ) internal pure returns (uint256[] memory) {
-        uint256 mMax = (startStrike - minStrike) / increment;
-        uint256 nMax = (maxStrike - startStrike) / increment;
-        uint256 totalStrikes = mMax + nMax + 1;
+        // Calculate number of strikes below and above startStrike
+        uint256 strikesBelow = (startStrike - minStrike) / increment;
+        uint256 strikesAbove = (maxStrike - startStrike) / increment;
+        uint256 totalStrikes = strikesBelow + strikesAbove + 1;
 
         uint256[] memory strikePrices = new uint256[](totalStrikes);
         
         // Generate strikes below startStrike
-        for (uint256 i = 0; i <= mMax; i++) {
-            strikePrices[i] = startStrike - (mMax - i) * increment;
+        for (uint256 i = 0; i <= strikesBelow; i++) {
+            strikePrices[i] = startStrike - (strikesBelow - i) * increment;
         }
         
         // Generate strikes above startStrike
-        for (uint256 i = 0; i < nMax; i++) {
-            strikePrices[mMax + 1 + i] = startStrike + (i + 1) * increment;
+        for (uint256 i = 0; i < strikesAbove; i++) {
+            strikePrices[strikesBelow + 1 + i] = startStrike + (i + 1) * increment;
         }
 
         return strikePrices;
+    }
+
+    function _generateCategory0StrikePrices(
+        uint256 atmStrike,
+        uint256 minStrike,
+        uint256 maxStrike,
+        uint256 baseIncrement
+    ) internal pure returns (uint256[] memory) {
+        // Calculate granular range boundaries (±5% from ATM)
+        uint256 lowerGranularBoundary = _round(atmStrike * 95 / 100, baseIncrement);
+        uint256 upperGranularBoundary = _round(atmStrike * 105 / 100, baseIncrement);
+
+        // Calculate number of strikes in each range
+        uint256 strikesLower = (lowerGranularBoundary - minStrike) / baseIncrement;
+        uint256 strikesMiddle = (upperGranularBoundary - lowerGranularBoundary) / (baseIncrement / 2);
+        uint256 strikesUpper = (maxStrike - upperGranularBoundary) / baseIncrement;
+        uint256 totalStrikes = strikesLower + strikesMiddle + strikesUpper + 1;
+
+        uint256[] memory strikePrices = new uint256[](totalStrikes);
+        uint256 currentIndex = 0;
+
+        // Generate strikes in lower range (standard increment)
+        for (uint256 i = 0; i < strikesLower; i++) {
+            strikePrices[currentIndex++] = minStrike + (i + 1) * baseIncrement;
+        }
+
+        // Generate strikes in middle range (half increment)
+        for (uint256 i = 0; i <= strikesMiddle; i++) {
+            strikePrices[currentIndex++] = lowerGranularBoundary + i * (baseIncrement / 2);
+        }
+
+        // Generate strikes in upper range (standard increment)
+        for (uint256 i = 0; i < strikesUpper; i++) {
+            strikePrices[currentIndex++] = upperGranularBoundary + (i + 1) * baseIncrement;
+        }
+
+        return strikePrices;
+    }
+
+    function _round(uint256 number, uint256 interval) private pure returns(uint256) {
+        uint256 mod = number % interval;
+        if (mod >= (interval / 2)) {
+            return ((number / interval) + 1) * interval;
+        } else {
+            return (number / interval) * interval;
+        }
     }
 }
--- test/StrikePrices.t.sol
+++ test/StrikePrices.t.sol
@@ -11,33 +11,33 @@ contract StrikePricesTest is Test {
 
     uint256[] private currentPrices = [
         1035 * 10**18,
-        1984 * 10**18
-        // 1125 * 10**18,
-        // 13625 * 10**17,
-        // 1800 * 10**18,
-        // 2250 * 10**18,
-        // 2825 * 10**18,
-        // 2940 * 10**18,
-        // 3575 * 10**18,
-        // 3755 * 10**18,
-        // 4500 * 10**18,
-        // 5650 * 10**18,
-        // 7150 * 10**18,
-        // 9000 * 10**18,
-        // 11250 * 10**18,
-        // 1000 * 10**18,
-        // 2000 * 10**18,
-        // 4000 * 10**18,
-        // 28000 * 10**18,
-        // 1560323 * 10**18
+        1984 * 10**18,
+        1125 * 10**18,
+        13625 * 10**17,
+        1800 * 10**18,
+        2250 * 10**18,
+        2825 * 10**18,
+        2940 * 10**18,
+        3575 * 10**18,
+        3755 * 10**18,
+        4500 * 10**18,
+        5650 * 10**18,
+        7150 * 10**18,
+        9000 * 10**18,
+        11250 * 10**18,
+        1000 * 10**18,
+        2000 * 10**18,
+        4000 * 10**18,
+        28000 * 10**18,
+        1560323 * 10**18
     ];
 
     uint32[] private timeIntervals = [
-        2 days,      // 2 days
-        14 days,     // 2 weeks
-        60 days,     // 2 months
-        180 days,     // 6 months
-        200 days     // 6.5 months
+        2 days     // 2 days
+        // 14 days,     // 2 weeks
+        // 60 days,     // 2 months
+        // 180 days,     // 6 months
+        // 200 days     // 6.5 months
     ];
 
     function testPrintStrikePrices() public view {
@@ -49,15 +49,16 @@ contract StrikePricesTest is Test {
 
             for (uint256 j = 0; j < timeIntervals.length; j++) {
                 uint32 expirationTime = currentTimestamp + timeIntervals[j];
-                // console2.log("\nTime to expiration:", timeIntervals[j] / 1 days, "days");
+                console2.log("\nTime to expiration:", timeIntervals[j] / 1 days, "days");
 
                 uint256[] memory strikes = StrikePrices.getStrikePrices(currentPrice, expirationTime);
+            
                 
-                // console2.log("Number of strikes:", strikes.length);
-                // console2.log("Strike prices:");
-                // for (uint256 k = 0; k < strikes.length; k++) {
-                //     console2.log(strikes[k] / 10**18);
-                // }
+                console2.log("Number of strikes:", strikes.length);
+                console2.log("Strike prices:");
+                for (uint256 k = 0; k < strikes.length; k++) {
+                    console2.log(strikes[k] / 10**18);
+                }
             }
         }
     }
